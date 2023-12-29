1579793a4750bf78b9017666ebdb8b508735814e
Thomas Negeli <t.negeli@limesoda.com>
2023-12-29 20:36:38 +0100

    Convert dYdX CSVs into arrays

--- src/convert.php
+++ src/convert.php
@@ -4,7 +4,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\SingleCommandApplication;
 
@@ -16,6 +15,45 @@ use Symfony\Component\Console\SingleCommandApplication;
     ->addArgument('transfers', InputArgument::REQUIRED, 'The dYdX transfers export file!')
     ->setCode(function (InputInterface $input, OutputInterface $output): int {
 
+        $trades = $input->getArgument('trades');
+        $trades = readCsv($trades);
+
+        $funding = $input->getArgument('funding');
+        $funding = readCsv($funding);
+
+        $transfers = $input->getArgument('transfers');
+        $transfers = readCsv($transfers);
+        
         return 0;
     })
     ->run();
+
+function readCsv($file): array
+{
+    $fp = @fopen($file, "r");
+    $lines = array();
+    $header = array();
+    if ($fp) {
+        $row = 0;
+        while (($buffer = fgets($fp)) !== false) {
+            $buffer = trim($buffer, "\r\n");
+            if ($row == 0) {
+                $header = str_getcsv($buffer);
+            } else {
+                $columns = str_getcsv($buffer);
+                $data = array();
+                foreach ($columns as $index => $column) {
+                    $data[$header[$index]] = $column;
+                    $data['csv_row'] = $row - 1;
+                }
+                $lines[] = $data;
+            }
+            $row++;
+        }
+        if (!feof($fp)) {
+            echo "Error: unexpected fgets() fail\n";
+        }
+        fclose($fp);
+    }
+    return $lines;
+}
